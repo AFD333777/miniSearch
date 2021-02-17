5fe300815358d1837bbec40e4c637f623ad5312d
AFD333777 <62431888+AFD333777@users.noreply.github.com>
2021-02-17 18:34:25 +0300

    Большая задача по Maps API. Часть №2

--- Draw.py
+++ Draw.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from MainWindow import Ui_MainWindow
 from RequestsSystem import RequestsSystem
+import os
 
 
 class MiniSearch(QMainWindow, Ui_MainWindow):
@@ -11,19 +12,24 @@ class MiniSearch(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.requestsSystem = RequestsSystem()
         self.setFixedSize(800, 600)
-        self.showMapbtn.clicked.connect(lambda: self.showMap("map", "pmwtm"))
+        self.showMapbtn.clicked.connect(lambda: self.showMap(
+            [self.lon.text(), self.lat.text()], self.scale.text(), "map"))
 
-    def showMap(self, viewMap="map", mark=""):
+    def showMap(self, coords, scale, viewMap="map", mark=""):
         # валидаторы
         if not (self.lon.text() and self.lat.text() and self.scale.text()) != "":
             self.labelErrorData.setText("Введите данные")
         else:
+            self.currentCoords = coords
+            self.currentScale = scale
+            self.currentViewMap = viewMap
+            self.currentMark = mark
             self.labelErrorData.setText("")
             self.labelError.setText("")
             self.mapFile = "map.png"
             with open(self.mapFile, "wb") as file:
                 self.respone = self.requestsSystem.getMap(
-                    [self.lon.text(), self.lat.text()], self.scale.text(), viewMap, mark)
+                    self.currentCoords, self.currentScale, self.currentViewMap, self.currentMark)
                 if self.respone.ok:
                     file.write(self.respone.content)
                 else:
@@ -33,6 +39,15 @@ class MiniSearch(QMainWindow, Ui_MainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
-            print("увеличение карты")
+            if 0 <= int(self.scale.text()) < 17:
+                self.currentScale = str(int(self.currentScale) + 1)
+                self.scale.setText(self.currentScale)
+                self.showMap(self.currentCoords, self.currentScale, self.currentViewMap, self.currentMark)
         if event.key() == Qt.Key_PageDown:
-            print("уменьшение карты")
+            if 0 <= int(self.scale.text()) <= 17:
+                self.currentScale = str(int(self.currentScale) - 1)
+                self.scale.setText(self.currentScale)
+                self.showMap(self.currentCoords, self.currentScale, self.currentViewMap, self.currentMark)
+
+    def closeEvent(self, event):
+        os.remove(self.mapFile)
